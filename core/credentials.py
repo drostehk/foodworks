e1c3e56e852dd373d21e7add382882b3dc6c3ca7
Mart van de Ven <m@type.hk>
2016-04-07 16:32:37 +0800

    Support oauth2client v2.0.0

--- core/credentials.py
+++ core/credentials.py
@@ -1,10 +1,9 @@
 __author__ = 'io'
 
-import json
-from oauth2client.client import SignedJwtAssertionCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Authenticate with Google, see http://gspread.readthedocs.org/en/latest/oauth2.html
 def getGoogleCredentials():
-    json_key = json.load(open('e.json'))
     scope = ['https://spreadsheets.google.com/feeds']
-    return SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('core/e.json', scope)
+    return credentials
