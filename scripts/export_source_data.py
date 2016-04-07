c4b23566209b7cfff8995e96965c94e122c19553
Mart van de Ven <m@type.hk>
2016-04-08 07:26:25 +0800

    Just retry if Google API craps out

--- scripts/export_source_data.py
+++ scripts/export_source_data.py
@@ -6,6 +6,7 @@ import os
 import sys
 import time
 import json
+from gspread.exceptions import HTTPError
 
 sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
 
@@ -48,48 +49,53 @@ def check_or_set(base, key):
 /SAVE PROGRESS
 '''
 
-for stage, ngos in generate_structure().iteritems():
+def export_source_sheets():
+    try:
+        for stage, ngos in generate_structure().iteritems():
 
-    check_or_set(progress, stage)
+            check_or_set(progress, stage)
 
-    if stage in ['distribution', 'processing']:
-        continue
-    
-    for ngo, programmes in ngos.iteritems():
-
-        if ngo in ['FoodLink', 'NLPRA']:
-            continue
-
-        check_or_set(progress[stage], ngo)
-
-        if not programmes:
-            continue
-    
-        for programme, sheets in programmes.iteritems():
-
-            if progress_completed(stage, ngo, programme):
-                print('\n>>> SKIPPING >>> ', ngo, stage.capitalize(), programme, ' >>> ', len(sheets), 'Yrs')
+            if stage in ['distribution', 'processing']:
                 continue
-
-            print('\n>>> ', ngo, stage.capitalize(), programme, ' >>> ', len(sheets), 'Yrs')
             
-            if not sheets:
-                continue
-    
-            for sheet in sheets:
-                ss = SheetToCanonical(**sheet)
-                
-                if stage == 'collection':
-                    ss.collection_sheets_to_csv()
-                    ss.donors_sheets_to_csv()
-                    ss.terms_sheets_to_csv()
-                
-                elif stage == 'processing':
-                    ss.finance_sheets_to_csv()
-                    ss.processing_sheets_to_csv()
-                
-                elif stage == 'distribution':
-                    ss.distribution_sheets_to_csv()
-                    ss.beneficiary_sheets_to_csv()
+            for ngo, programmes in ngos.iteritems():
 
-            check_or_set(progress[stage][ngo], programme)
+                if ngo in ['FoodLink', 'NLPRA']:
+                    continue
+
+                check_or_set(progress[stage], ngo)
+
+                if not programmes:
+                    continue
+            
+                for programme, sheets in programmes.iteritems():
+
+                    if progress_completed(stage, ngo, programme):
+                        print('\n>>> SKIPPING >>> ', ngo, stage.capitalize(), programme, ' >>> ', len(sheets), 'Yrs')
+                        continue
+
+                    print('\n>>> ', ngo, stage.capitalize(), programme, ' >>> ', len(sheets), 'Yrs')
+                    
+                    if not sheets:
+                        continue
+            
+                    for sheet in sheets:
+                        ss = SheetToCanonical(**sheet)
+                        
+                        if stage == 'collection':
+                            ss.collection_sheets_to_csv()
+                            ss.donors_sheets_to_csv()
+                            ss.terms_sheets_to_csv()
+                        
+                        elif stage == 'processing':
+                            ss.finance_sheets_to_csv()
+                            ss.processing_sheets_to_csv()
+                        
+                        elif stage == 'distribution':
+                            ss.distribution_sheets_to_csv()
+                            ss.beneficiary_sheets_to_csv()
+
+                    check_or_set(progress[stage][ngo], programme)
+    
+    except HTTPError:
+        export_source_sheets()
